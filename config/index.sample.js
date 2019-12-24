cc99a9ca1bbe203038ceb75d2602b6ac883b89e6
jian652485051@163.com <jian652485051@163.com>
2019-12-24 15:20:27 +0800

    demo使用开放的mock接口

--- config/index.sample.js
+++ config/index.sample.js
@@ -1,9 +1,9 @@
 const URLHASH = {
-  mock: 'rap2server.sooc.com/app/mock/23',
+  mock: 'rap2api.taobao.org/app/mock/163112',
   dev: 'esb.sooc.com',                        //开发接口域名
   test: 'mp.esb.sooc.com',                    //测试接口域名
   pre: 'ttesbmp.soocedu.com',                 //预上线接口域名
   master: 'esbmp.soocedu.com'                 //正式接口域名
 };
 
-export const api = 'http://'+URLHASH.dev;
+export const api = 'http://'+URLHASH.mock;
